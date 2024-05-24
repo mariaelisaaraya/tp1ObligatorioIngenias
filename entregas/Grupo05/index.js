a477a3877fc9839caeca4637c52be8bcd3c4a90f
SoleCisnero3 <solecisnero3@gmail.com>
2024-05-24 10:05:16 -0300

    empezando

--- entregas/Grupo05/index.js
+++ entregas/Grupo05/index.js
@@ -1,1 +1,26 @@
-console.log("Hola chicas");
+const http = require('http');
+const PORT = 3008;
+
+
+const server = http.createServer((request, response) =>{
+    let respuesta = "";
+    let statusCode = 200;
+    if(request.url ==='/'){
+        respuesta = "Bienvenid@s a nuestro primer trabajo practico."
+    }else if(request.url ==='/catalogo'){
+
+        }else if(request.url ==='/titulo/:title'){
+
+          }else if(request.url ==='/categoria/:cat'){
+
+          }else if(request.url ==='/reparto/:act'){
+
+          }else if(request.url ==='/trailer/:id'){
+
+          }else {
+            statusCode = 404;
+            respuesta = "No se encontro la ruta, vuelve a intentarlo.";
+
+          }
+    
+})
