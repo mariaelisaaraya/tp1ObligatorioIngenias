b1c8517e8fdb626dd17f1710bebf03022f2878c3
Quimey <quimeymalen@live.com.ar>
2024-05-26 22:42:03 -0300

    subo de vuelta

---
+++ entregas/Grupo09/subo los archivos/src/trailerflix.controller.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+function leerTrailerflix(){ 
+    const filePath = path.join(__dirname, process.env.DATABASE_PATH);
+    const datos = fs.readFileSync(filePath, 'utf-8' )
+    const TRAILERFLIX = JSON.parse(datos)
+    return TRAILERFLIX
+} 
+module.exports = {leerTrailerflix}
+// FUNCION ENCARGADA DE TRANSFORMAR LOS DATOS PARA USARLOS
+
